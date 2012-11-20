bfbafde75f0080c10ed173af75d0057f3236e807
Santiago Pavon <santiago@dit.upm.es>
2012-11-20 17:30:53 +0100

    Movie uploading is working and using auth from User Defaults.

--- iOS/ViSH Mobile/ViSH Mobile/FormViewController.m
+++ iOS/ViSH Mobile/ViSH Mobile/FormViewController.m
@@ -53,81 +53,90 @@
 
 - (void) createPost_VISH {
     
-    // Dictionary that holds then basic authentication data.
-    NSDictionary* basicAuth = @{
-    @"email": @"PONER_USER@dit.upm.es",
-    @"password": @"PONER_PASSWORD"
-    };
+    //-- create request
     
-    // Dictionary that holds post parameters.
-    // You can set your post parameters that your server accepts or programmed to accept.
-    NSDictionary* _params = @{
-    @"document[file]": @"movie.mov",
-    @"document[title]": self.titleField.text,
-    @"document[description]": self.bodyField.text
-    };
-    
-    
-    // the boundary string : a random string, that will not repeat in post data, to separate post data fields.
-    NSString *BoundaryConstant = @"----------V2ymHFg03ehbqgZCaKO6jy";
-    
-    // string constant for the post parameter 'file'. My server uses this name: `file`. Your's may differ
-    NSString* FileParamConstant = @"document[file]";
-    
-    // create request
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
     [request setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
     [request setHTTPShouldHandleCookies:NO];
     [request setTimeoutInterval:30];
     [request setHTTPMethod:@"POST"];
     
-    // set Basic authentication in HTTP header
-    NSString *user_password = [NSString stringWithFormat:@"%@:%@", basicAuth[@"email"], basicAuth[@"password"]];
+    //-- HTTP header: Basic authentication
+    
+    // Get authentication data from User Defaults:
+    NSUserDefaults *def = [NSUserDefaults standardUserDefaults];
+    NSString * email = [def stringForKey:@"email"];
+    NSString * password = [def stringForKey:@"password"];
+    
+    // set the HTTP header:
+    NSString *user_password = [NSString stringWithFormat:@"%@:%@", email, password];
     NSData * user_password_data = [user_password dataUsingEncoding:NSUTF8StringEncoding];
     NSString * u_p_d_base64 = [user_password_data base64EncodedString];
     NSString *basic_auth_value = [NSString stringWithFormat:@"Basic %@", u_p_d_base64];
     [request setValue:basic_auth_value forHTTPHeaderField:@"Authorization"];
     
-    // set Content-Type in HTTP header
-    NSString * boundary = BoundaryConstant;
+    //-- HTTP header: Content-Type is form multipart  
+    
+    // the boundary string : a random string, that will not repeat in post data, to separate post data fields.
+    NSString *boundary = @"----------V2ymHFg03ehbqgZCaKO6jy";
+    
     NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",boundary];
     [request setValue:contentType forHTTPHeaderField: @"Content-Type"];
     
-    // post body
+    //-- post body
+    
     NSMutableData *body = [NSMutableData data];
+
+    // Dictionary that holds post parameters.
+    NSDictionary* _params = @{
+    @"document[title]": self.titleField.text,
+    @"document[description]": self.bodyField.text
+    };
     
     // add params (all params are strings)
     for (NSString *param in _params) {
-        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
-        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", param] dataUsingEncoding:NSUTF8StringEncoding]];
-        [body appendData:[[NSString stringWithFormat:@"%@\r\n", [_params objectForKey:param]] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary]
+                          dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", param]
+                          dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"%@\r\n", [_params objectForKey:param]]
+                          dataUsingEncoding:NSUTF8StringEncoding]];
     }
     
     
+    // name of the post parameter for the file to upload:
+    NSString* fileParamName = @"document[file]";
     
     
     // add movie data
     NSData *movieData = [NSData dataWithContentsOfURL:self.movie];
     if (movieData) {
-        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"movie.mov\"\r\n", FileParamConstant] dataUsingEncoding:NSUTF8StringEncoding]];
-        [body appendData:[@"Content-Type: video/quicktime\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary]
+                          dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"movie.mov\"\r\n", fileParamName]
+                          dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[@"Content-Type: video/quicktime\r\n\r\n"
+                          dataUsingEncoding:NSUTF8StringEncoding]];
         [body appendData:movieData];
-        [body appendData:[[NSString stringWithFormat:@"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"\r\n"]
+                          dataUsingEncoding:NSUTF8StringEncoding]];
     }
     
-    [body appendData:[[NSString stringWithFormat:@"--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    // The end of the multipart body
+    [body appendData:[[NSString stringWithFormat:@"--%@--\r\n", boundary]
+                      dataUsingEncoding:NSUTF8StringEncoding]];
     
     // setting the body of the post to the reqeust
     [request setHTTPBody:body];
     
-    // set the content-length
+    
+    //-- HTTP header: Content-length
+    
     NSString *postLength = [NSString stringWithFormat:@"%d", [body length]];
     [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
     
-    // set URL
-    // the server url to which the image (or the media) is uploaded.
-    // Use your server url here
+    //-- the URL
+
     NSURL* requestURL = [NSURL URLWithString:VISH_URL];
     [request setURL:requestURL];
     
